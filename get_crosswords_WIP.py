38da97ae73e6ed8193f327c70bdd11ca50174482
Richard Neish <richardn@richardneish.org>
2011-09-05 22:17:08 +0100

    WIP - fixed compile errors in script, added download() method, started turning script into module.

--- get_crosswords_WIP.py
+++ get_crosswords_WIP.py
@@ -21,9 +21,18 @@ def login():
     br["loginDetails[email]"] = email
     br["loginDetails[password]"] = password
     response = br.submit()
+    return response
 
 def convert_crossword(tree):
-    log.error("'convert_crossword()' Not implemented!!")
+    #for row in tree.xpath("//table[2]//tr[2]//table//tr"):
+	    
+    return tree
+
+def load_file(title):
+    """Load a previously saved XHTML file, and build the parse tree """
+    file = open(path.join(output_dir, title + ".html"), "r")
+    tree = etree.parse(file.read(), parser)
+    file.close()
     return tree
 
 def save_file(title, tree):
@@ -32,32 +41,39 @@ def save_file(title, tree):
     file.write(etree.tostring(tree.getroot(), pretty_print=True, method="xml"))
     file.close()
 
-"""Configure logging """
-logging.basicConfig(level=logging.DEBUG, stream=sys.stderr, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-log = logging.getLogger("get_crosswords")
+def download_crossword(tree, type):
+    title = tree.xpath("//div[@id='latest_games']//p[starts-with(text(), " +
+                       "'" + type + "')]/text()")[0]
+    link = tree.xpath("//div[@id='latest_games']//p[starts-with(text(), " +
+                      "'" + type + "')]/following-sibling::a[1]/@href")[0]
+    log.info("Downloading puzzle '" + title + "' from '" + link + "'.")
+    response = br.open(link)
+    crossword_tree = etree.parse(StringIO(response.get_data()), parser)
+    return (title, crossword_tree)
 
-log.debug("Get configuration from .rc file.")
-config = ConfigParser({"directory" : "~/Documents/crosswords"})
-config.read(path.expanduser("~/.get_crosswords.rc"))
-email = config.get("Credentials", "email")
-password = config.get("Credentials", "password")
-output_dir = path.expanduser(config.get("Output", "directory"))
 
-br = mechanize.Browser()
-parser = etree.HTMLParser()
-response = login()
-log.debug("Parse the page and extract the puzzle links.")
-tree = etree.parse(StringIO(response.get_data()), parser)
-
-log.debug("Download the puzzles and reparse into the local form.")
-for type in ["QUICK", "CRYPTIC"]:
-  title = tree.xpath("//div[@id='latest_games']//p[starts-with(text(), " +
-                     "'" + type + "')]/text()")[0]
-  link = tree.xpath("//div[@id='latest_games']//p[starts-with(text(), " +
-                          "'" + type + "')]/following-sibling::a[1]/@href")[0]
-  log.info("Downloading puzzle '" + title + "' from '" + link + "'.")
-  response = br.open(link)
-  crossword_tree = etree.parse(StringIO(response.get_data()), parser)
-  local_format = convert_crossword(crossword_tree)
-  save_file(title, local_format)
+######## MAIN PROGRAM ######
+if __name__ == "__main__":
+    """Configure logging """
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stderr, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    log = logging.getLogger("get_crosswords")
+    
+    log.debug("Get configuration from .rc file.")
+    config = ConfigParser({"directory" : "~/Documents/crosswords"})
+    config.read(path.expanduser("~/.get_crosswords.rc"))
+    email = config.get("Credentials", "email")
+    password = config.get("Credentials", "password")
+    output_dir = path.expanduser(config.get("Output", "directory"))
+    
+    br = mechanize.Browser()
+    parser = etree.HTMLParser()
+    response = login()
+    log.debug("Parse the page and extract the puzzle links.")
+    tree = etree.parse(StringIO(response.get_data()), parser)
+    
+    log.debug("Download the puzzles and reparse into the local form.")
+    for type in ["QUICK", "CRYPTIC"]:
+        (title, crossword_tree) = download_crossword(tree)
+        local_format = convert_crossword(crossword_tree)
+        save_file(title, local_format)
 
