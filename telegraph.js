c1dfd5d3b310ce3d5d341e6141630ef821415a9e
Richard Neish <richardn@richardneish.org>
2012-12-17 15:06:29 +0000

    PhantomJS scraper - breaking in submitLoginForm()

--- telegraph.js
+++ telegraph.js
@@ -1,33 +1,36 @@
-if (!phantom.injectJs('async.min.js')) {
-  exit('Failed to load async library.');
+'use strict';
+function log(msg) {
+  console.log(msg);
 }
 
-var env = require('system').env;
-var fs = require('fs');
-var page = require('webpage').create();
-
-// var LOGIN_URL = 'file:///c:/temp/mock.html';
-var LOGIN_URL = 'http://puzzles.telegraph.co.uk/site/index.php';
-
 function debug(msg) {
-  console.log('DEBUG: ' + msg);
+  log('DEBUG: ' + msg);
 }
 
 function exit(msg) {
   if (msg) {
-    console.log(msg);
+    log(msg);
     phantom.exit(1);
   } else {
     phantom.exit(0);
   }
 }
 
+if (!phantom.injectJs('async.min.js')) {
+  exit('Failed to load async library.');
+}
+
+// var LOGIN_URL = 'file:///c:/temp/mock.html';
+var LOGIN_URL = 'http://puzzles.telegraph.co.uk/site/index.php';
+
 function loadConfig() {
   try {
-    var baseDir = env['HOME'] || '.';
-    var configFile = baseDir + '/.telegraphrc';
-    var is = fs.open(configFile, 'r');
-    var config = eval(is.read());
+    var env = require('system').env,
+      fs = require('fs'),
+      baseDir = env.HOME || '.',
+      configFile = baseDir + '/.telegraphrc',
+      is = fs.open(configFile, 'r'),
+      config = eval(is.read());
     is.close();
     return config;
   } catch (e) {
@@ -35,21 +38,24 @@ function loadConfig() {
   }
 }
 
+var page = require('webpage').create(),
+  config = loadConfig();
+
 function setEventHandlers() {
   page.settings.userAgent = 'Mozilla/5.0 (Windows NT 5.1; rv:9.0) Gecko/20100101 Firefox/9.0';
-  page.onAlert = function(msg) {
-    console.log('ALERT: ' + msg);
+  page.onAlert = function (msg) {
+    log('ALERT: ' + msg);
   };
 
-  page.onConsoleMessage = function(msg, lineNum, sourceId) {
-    console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
+  page.onConsoleMessage = function (msg, lineNum, sourceId) {
+    log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
   };
 
-  page.onError = function(msg, trace) {
+  page.onError = function (msg, trace) {
     var msgStack = ['ERROR: ' + msg];
     if (trace) {
       msgStack.push('TRACE:');
-      trace.forEach(function(t) {
+      trace.forEach(function (t) {
         msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
       });
     }
@@ -57,108 +63,142 @@ function setEventHandlers() {
     page.render('error.png');
   };
 
-  page.onNavigationRequested = function(url, type, willNavigate, main) {
-    console.log('Trying to navigate to: ' + url);
-    console.log('Caused by: ' + type);
-    console.log('Will actually navigate: ' + willNavigate);
-    console.log('Sent from the page\'s main frame: ' + main);
-  }
+  page.onNavigationRequested = function (url, type, willNavigate, main) {
+    log('Trying to navigate to: ' + url);
+    log('Caused by: ' + type);
+    log('Will actually navigate: ' + willNavigate);
+    log('Sent from the page\'s main frame: ' + main);
+  };
 
-  page.onLoadFinished = function(status) {
+  page.onLoadFinished = function (status) {
     debug('loadFinished(' + status + ')');
-  }
+  };
 
 }
 
 function waitFor(testFx, onReady, timeOutMillis) {
-  var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3000, //< Default Max Timout is 3s
-      start = new Date().getTime(),
-      condition = false,
-      interval = setInterval(function() {
-        if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
-          // If not time-out yet and condition not yet fulfilled
-          condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
+  var maxtimeOutMillis = timeOutMillis || 3000, //< Default Max Timout is 3s
+    start = new Date().getTime(),
+    condition = false,
+    interval = setInterval(function () {
+      if ((new Date().getTime() - start < maxtimeOutMillis) && !condition) {
+        // If not time-out yet and condition not yet fulfilled
+        condition = typeof testFx === "string" ? eval(testFx) : testFx(); //< defensive code
+      } else {
+        if (!condition) {
+          // If condition still not fulfilled (timeout but condition is 'false')
+          log("'waitFor()' timeout");
+          phantom.exit(1);
         } else {
-          if(!condition) {
-            // If condition still not fulfilled (timeout but condition is 'false')
-            console.log("'waitFor()' timeout");
-            phantom.exit(1);
+          // Condition fulfilled (timeout and/or condition is 'true')
+          log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
+          // Do what it's supposed to do once the condition is fulfilled
+          if (typeof onReady === "string") {
+            eval(onReady);
           } else {
-            // Condition fulfilled (timeout and/or condition is 'true')
-            console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
-            typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
-            clearInterval(interval); //< Stop this interval
+            onReady();
           }
+          clearInterval(interval); //< Stop this interval
         }
-      }, 250); //< repeat check every 250ms
+      }
+    }, 250); //< repeat check every 250ms
 }
 
+function saveError() {
+  var fs = require('fs'),
+    os = fs.open('error.html', 'w');
+  os.write(page.content);
+  os.close();
+  page.render('error.png');
+}
 
 function openLoginForm(next) {
   debug('openLoginForm');
-  page.open(LOGIN_URL, function(status) {
+  page.open(LOGIN_URL, function (status) {
     if (status !== 'success') {
       next('Failed to open url \'' + LOGIN_URL + '\'.');
     } else {
       next(null);
     }
   });
-}  
+}
 
 function submitLoginForm(next) {
   debug('submitLoginForm()');
-  page.evaluate(function(username, password) {
-    var email = document.getElementById('email');
-    var password = document.getElementById('password');
-    email.value = username;
-    password.value = password;
+  if (!page.evaluate(function (config_username, config_password) {
+    var email = document.getElementById('email'),
+      password = document.getElementById('password');
+    if (!email) {
+      console.log('Email element not found.');
+      return false;
+    }
+    if (!password) {
+      console.log('Password element not found.');
+      return false;
+    }
+    email.value = config_username;
+    password.value = config_password;
     document.login.submit();
-  }, config['username'], config['password']);
-  next(null);
+    return true;
+  }, config.username, config.password)) {
+    saveError();
+    next('submitLoginForm() failed.');
+  } else {
+    next(null);
+  }
 }
 
 function waitForLogin(next) {
+  var waiting = true,
+    intervalId = setInterval(function () {
+    clearInterval(intervalId);
+    waiting = false;
+  }, 5000
+  );
   debug('waitForLogin');
   waitFor(
-    function() {
-      return page.evaluate(function() {
+    function () {
+      return !waiting && page.evaluate(function () {
+        var result = false;
         if (document.getElementById('welcome_message')) {
-          return 'success';
+          result = 'success';
         } else if (document.getElementById('popup-login-err')) {
-          return 'failed';
+          result = 'failed';
         } else {
-          return false;
+          result = false;
         }
+        return result;
       });
     },
-    function(result) {
-      console.log('Login result is: \'' + result + '\'.')
-      if (result == 'success') {
+    function (result) {
+      log('Login result is: \'' + result + '\'.');
+      if (result === 'success') {
         next(null);
       } else {
-        var os = fs.open('error.html', 'w');
-        os.write(page.content);
-        os.close();
+        saveError();
         next('Login failed');
       }
     },
-    30000);
+    30000
+  );
 }
 
 //
 // Main program.
 //
-setInterval(function() {exit('Timeout')}, 30000);
-var config = loadConfig();
+setInterval(function () {
+  exit('Timeout');
+}, 30000);
+
 setEventHandlers();
 try {
   async.waterfall([
     openLoginForm,
     submitLoginForm,
     waitForLogin
-    ], exit);
+  ], exit);
 } catch (e) {
-  console.log(e);
+  log(e);
   phantom.exit(0);
 }
-console.log('Starting...');
+log('Starting...');
